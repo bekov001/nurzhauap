e65dacec0895881dd1a614943ac3111a8e995278
bekov001 <79100238+bekov001@users.noreply.github.com>
2021-09-30 22:21:17 +0600

    return back

--- example/settings.py
+++ example/settings.py
@@ -140,3 +140,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals()) 
