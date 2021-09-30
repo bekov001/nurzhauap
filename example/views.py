1c133a7cfff212d023eafdf67d72750ddac8b8eb
bekov001 <79100238+bekov001@users.noreply.github.com>
2021-09-30 20:55:27 +0600

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
-def page_not_found_view(request, exception):
-    return render(request, '404/index.html', status=404)
+# def page_not_found_view(request, exception):
+#     return render(request, '404/index.html', status=404)
